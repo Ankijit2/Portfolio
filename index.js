77630ea803904d6f35319a0b194d6e5bfe853c44
ANKIJIT  ROY <149407832+Ankijit2@users.noreply.github.com>
2023-12-23 18:15:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import 'dotenv/config'
 const app = express();
-const port = 3000;
+const port =process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
